7fb7e9ea0e1f3f10ee60df38ed5c9cd8faef7acd
Emmar Caber <caberemmar@gmail.com>
2024-12-20 20:55:27 +0800

    Initial commit

---
+++ students/caber.js
@@ -0,0 +1,7 @@
+export const studentInformation = {
+  name: "Emmar Caber",
+  githubUsername: "emmarcaber",
+  email: "caberemmar@gmail.com",
+  github: "https://github.com/emmarcaber",
+  githubImageLink: "https://avatars.githubusercontent.com/u/71781006?v=4",
+};
